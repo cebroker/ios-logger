a8bfb80be9e625e12583d54d59b9ddcf13c5572a
Carlos Pava <cpava@condorlabs.io>
2019-09-30 09:20:30 -0500

    remove pods and add .ignore

--- Pods/Headers/Public/Logger/Logger-umbrella.h
+++
@@ -1,1 +0,0 @@
-../../../Target Support Files/Logger/Logger-umbrella.h
--- Pods/Target Support Files/Logger/Logger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Logger : NSObject
-@end
-@implementation PodsDummy_Logger
-@end
